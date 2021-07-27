91e4d051e5859f340ad7385247766e58c27b707a
bmanek <41875460+bmanek@users.noreply.github.com>
2021-07-27 09:38:53 -0400

    add associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
-  get 'jobs/new'
-  get 'jobs/create'
-  get 'jobs/show'
-  get 'jobs/edit'
-  get 'jobs/update'
-  get 'jobs/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
+
+  resources :jobs
+  # get 'jobs/index'
+  # get 'jobs/new'
+  # get 'jobs/create'
+  # get 'jobs/show'
+  # get 'jobs/edit'
+  # get 'jobs/update'
+  # get 'jobs/destroy'
+
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'users/update'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
